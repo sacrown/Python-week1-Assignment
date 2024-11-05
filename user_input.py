4d41814994cce9af446506cf68fb8cff93ba8870
sacrown <104371963+sacrown@users.noreply.github.com>
2024-11-05 20:56:49 +0100

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,8 @@
+
+# Asking for user input using the input() function
+name = input("Please enter your name: ")
+age = input("Please enter your age: ")
+location = input("Please enter your location: ")
+
+# Displaying a personalized message
+print(f"Hello {name}, you are {age} years old and live in {location}.")
